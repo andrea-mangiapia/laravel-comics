dd2ecde4781b1a63d06e7c3df94dd26002720355
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-08-25 17:02:56 +0200

    add array element to single comics

--- resources/views/single-comics.blade.php
+++ resources/views/single-comics.blade.php
@@ -5,12 +5,44 @@
 @endsection
 
 @section('main_content')
-    <div class="container">
-        <h1>
-            Ciao sono la pagina Single Comics
-        </h1>
-    
-        
-    </div>
+    <section class="banner-img">
+        <div class="container">
+            <img src="{{ $current_comics['thumb'] }}" alt="">            
+        </div>
+    </section>
+    <section class="comics-content">
+        <div class="container am-flex">
+            <div class="comics-text">
+                <h1>
+                    {{ $current_comics['title'] }}
+                </h1>
+
+                <div class="banner-price am-flex">
+                    <div class="price-item am-flex">
+                        <div class="price">
+                            <span class="text-style-price">U.S. Price:</span> {{ $current_comics['price'] }}
+                        </div>
+                        <div class="available">
+                            <span class="text-style-price">AVAILABLE</span>
+                        </div> 
+                    </div>
+                    <div class="check">
+                        Check Availability
+
+                    </div>
+                </div>
+
+                <div>
+                    <p>
+                        {{ $current_comics['description'] }}                        
+                    </p>
+                </div>
+                
+            </div>
+            <div class="adv">
+                <img src="../img/adv.jpg" alt="Advertisement">
+            </div>
+        </div>
+    </section>
     
 @endsection
