c790b9a03672b2add7e65904a79ec951f036836d
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-08-25 18:51:55 +0200

    done array element to single comics

--- resources/views/single-comics.blade.php
+++ resources/views/single-comics.blade.php
@@ -17,8 +17,8 @@
                     {{ $current_comics['title'] }}
                 </h1>
 
-                <div class="banner-price am-flex">
-                    <div class="price-item am-flex">
+                <div class="banner-price">
+                    <div class="price-item">
                         <div class="price">
                             <span class="text-style-price">U.S. Price:</span> {{ $current_comics['price'] }}
                         </div>
@@ -43,6 +43,86 @@
                 <img src="../img/adv.jpg" alt="Advertisement">
             </div>
         </div>
+    </section>
+
+    {{-- Comics Details --}}
+    <section>
+        <div class="container">
+            <div class="comics-details-content">
+                <div class="comics-details-text">
+                    <h3>
+                        Talent
+                    </h3>
+                    <div>
+                        <div>
+                            <div>
+                                Art by:
+                            </div>
+                            <div>
+                                @foreach ($current_comics['artists'] as $artist)
+                                    <span class="color-blue">
+                                        {{ $artist }}
+                                    </span>
+                                    @if(!$loop->last), @endif
+                                @endforeach
+                            </div>
+
+                        </div>
+                        <div>
+                            <div>
+                                Written by:
+                            </div>
+                            <div>
+                                @foreach ($current_comics['writers'] as $writers)
+                                    <span class="color-blue">
+                                        {{ $writers }}
+                                    </span>
+                                    @if(!$loop->last), @endif
+                                @endforeach
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="comics-details-text">
+                    <h3>
+                        Specs
+                    </h3>
+                    
+                    <div>
+                        <div>
+                            Series:
+                        </div>
+                        
+                        <div class="color-blue">
+                            {{ $current_comics['series'] }}
+                        </div>
+                    </div>
+        
+                    <div>
+                        <div>
+                            U.S. Price:
+                        </div>
+                        
+                        <div>
+                            {{ $current_comics['price'] }}
+                        </div>
+                    </div>
+        
+                    <div>
+                        <div>
+                            On Sale Date:
+                        </div>
+                        
+                        <div>
+                            {{ $current_comics['sale_date'] }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </section>
     
 @endsection
